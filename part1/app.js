9f7b584f089b649bb12618805c2d7f96e7e796d9
EzequielMejias <a1925635@adelaide.edu.au>
2025-06-21 01:12:41 +0000

    [automatic commit] 2025-06-21 01:12:41 UTC 175.35.253.182

--- part1/app.js
+++ part1/app.js
@@ -63,7 +63,8 @@ app.get('/api/walkrequests/open', (req, res) => {
 });
 
 app.get('/api/walkers/summary', (req,res) => {
-        db.query(`
+    try{ 
+    const query=`
             SELECT u.username AS walker_username,
                 COUNT(wr.rating) AS total_ratings,
                 AVG(wr.rating) AS average_rating,
