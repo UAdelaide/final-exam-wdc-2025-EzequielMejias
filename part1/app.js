d2252321bc79a10192350d98e889573c450e5cb1
EzequielMejias <a1925635@adelaide.edu.au>
2025-06-21 01:24:44 +0000

    [automatic commit] 2025-06-21 01:24:44 UTC 175.35.253.182

--- part1/app.js
+++ part1/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 const db = mysql.createConnection({
     host: 'localhost',
