4f4eda08e11d1518ea8a08f87390f9a437d06871
EzequielMejias <a1925635@adelaide.edu.au>
2025-06-21 06:43:48 +0000

    [automatic commit] 2025-06-21 06:43:48 UTC 175.35.253.182

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -66,7 +66,7 @@ router.post('/logout', (req, res) => {
     if(err) {
       return res.status(500).json({error: 'Logout failed'});
     }
-    res.json({message: 'Logged out'})
+    res.json({message: 'Logged out'});
   })
 })
 
